8a380d82c06742e15069037060a4a5f3d59de2b7
Vasylenko Olexandr <VassalX@users.noreply.github.com>
2018-05-27 22:17:48 +0300

    Homework1
    Rabit walks on platform

---
+++ Assets/HeroFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroRabit rabit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform rabit_tr = rabit.transform;
+		Transform camera_tr = this.transform;
+		Vector3 rabit_vec = rabit_tr.position;
+		Vector3 camera_vec = camera_tr.position;
+		camera_vec.x = rabit_vec.x;
+		camera_vec.y = rabit_vec.y;
+		GetComponent<Camera>().transform.position = camera_vec;
+	}
+}
